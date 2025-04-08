b6114605fd62099c2afa3e81b3389b2967f06dd1
Salohiddin827 <salohiddinbekmirzayev827@gmail.com>
2025-04-08 11:03:09 +0900

    password generator

--- password_generator.py
+++ password_generator.py
@@ -1,26 +1,25 @@
 import random
-
-lowc_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
-           'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-          ]
-upc_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import string
 
 print("Welcome to the Password Generator!")
-nr_up_letters = int(input("How many uppercase letters would you like to have in your password?\n"))
-nr_low_letters = int(input("How many lowercase letters would you like to have in your password?\n"))
+nr_uppercase_letters = int(input("How many uppercase letters would you like to have in your password?\n"))
+nr_lowercase_letters = int(input("How many lowercase letters would you like to have in your password?\n"))
 nr_symbols = int(input("How many symbols would you like to have?\n"))
 nr_numbers = int(input("How many numbers would you like to have?\n"))
 
+lowercase_letters = random.choices(string.ascii_lowercase, k=nr_lowercase_letters) 
+uppercase_letters =  random.choices(string.ascii_uppercase, k=nr_uppercase_letters) 
+numbers = random.choices(string.digits, k=nr_numbers)
+symbols = random.choices(string.punctuation, k = nr_symbols)
+
+
 password_list = []
 
-for _ in range(nr_up_letters):
-    password_list.append(random.choice(upc_letters))
+for _ in range(nr_uppercase_letters):
+    password_list.append(random.choice(uppercase_letters))
 
-for _ in range(nr_low_letters):
-    password_list.append(random.choice(lowc_letters))
+for _ in range(nr_lowercase_letters):
+    password_list.append(random.choice(lowercase_letters))
 
 for _ in range(nr_symbols):
     password_list.append(random.choice(symbols))
